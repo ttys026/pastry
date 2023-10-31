94e9bd1633ea1c75b6d194c45b850fe35a15ddfc
litianyi.troy <litianyi.troy@bytedance.com>
2023-10-31 14:57:12 +0800

    chore: use modified macocr binary

--- src/utils/ocr.ts
+++ src/utils/ocr.ts
@@ -3,7 +3,6 @@ import { cache } from "./storage";
 
 export const ocr = async (base64: string) => {
   const imgDir = `pastry_image_${Date.now()}_${Math.random()}`;
-  const outputDir = `pastry_output_${Date.now()}_${Math.random()}.txt`;
   await cache.set(
     imgDir,
     new Uint8Array(
@@ -12,25 +11,15 @@ export const ocr = async (base64: string) => {
         .map((char) => char.charCodeAt(0))
     )
   );
-  console.info({
-    imgDir,
-    outputDir,
-    a: await cache.patch(imgDir),
-    b: await cache.patch(outputDir),
-  });
   const command = Command.sidecar("binary/OCR", [
-    "zh-Hans,en-US",
-    "false",
-    "true",
+    "-l",
+    "zh-Hans",
+    "-i",
     await cache.patch(imgDir),
-    await cache.patch(outputDir),
   ]);
-  const { code } = await command.execute();
+  const { code, stdout } = await command.execute();
   if (code !== 0) {
     throw new Error("OCR 失败");
   }
-  console.info("????", code);
-  const output = await cache.get(outputDir);
-  cache.remove(outputDir);
-  return JSON.parse(output);
+  return stdout;
 };
